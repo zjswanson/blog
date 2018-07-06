6c15da3e0caf6d4f00d8a78599e283b8d90bdc8c
Zach Swanson <zach.j.swanson@gmail.com>
2018-07-06 10:40:03 -0700

    fix db connection in settings

--- zs_blog/settings.py
+++ zs_blog/settings.py
@@ -76,7 +76,12 @@ WSGI_APPLICATION = 'zs_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
